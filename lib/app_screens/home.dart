607a370cf07160cd5b2ed1c3924dbecf98e2e18c
Allanetizen <allankosgei747@gmail.com>
2020-05-25 06:01:53 -0700

    styling

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -10,7 +10,9 @@ class Home extends StatelessWidget{
      // width: 421.5,
       //height: 321.5,
       margin: EdgeInsets.only(right:15.0 , top: 20.5),
-      child: Text("Abracadabra",textDirection: TextDirection.ltr,),
+      child: Text("Abracadabra",
+         textDirection: TextDirection.ltr,
+      ),
     )
 
 
