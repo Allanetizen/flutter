c5109be3f1a62ebbbcd4dbc12af1bcca5b173742
Allanetizen <allankosgei747@gmail.com>
2020-05-25 23:31:04 +0300

    rows , columns and fonts

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,26 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class Home extends StatelessWidget{
+
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-        child:Container(
-      alignment: Alignment.bottomRight,
-      color: Colors.deepPurple,
-     // width: 421.5,
-      //height: 321.5,
-      margin: EdgeInsets.only(right:15.0 , top: 20.5),
-      child: Text("Abracadabra",
-         textDirection: TextDirection.ltr,
-      ),
-    )
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            // width: 421.5,
+            //height: 321.5,
+            margin: EdgeInsets.all(15.0),
+            child: Column(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Kenya Airways",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 30.0,
+                        fontFamily: 'Ubuntu',
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white),
+                  )),
+                  Expanded(
+                    child: Text("From London to Nairobi",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'Ubuntu',
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white)),
+                  ),
+                ]
+                ),
+                Row(children: <Widget>[
+                  Expanded(
+                      child: Text(
+                        "Fly 540",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 30.0,
+                            fontFamily: 'Ubuntu',
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white),
+                      )),
+                  Expanded(
+                    child: Text("From New Delhi to Kenya",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'Ubuntu',
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white)),
+                  ),
+                ]
+                )
 
-
-        );
-
-
-
-
+              ],
+            )));
   }
-
 }
