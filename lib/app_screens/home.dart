6b7c5e4f8f7bc74ccbe5ba175bc087d491eb7597
Allanetizen <allankosgei747@gmail.com>
2020-05-26 00:33:17 +0300

    Loading image assets

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,26 +1,81 @@
+
 import 'package:flutter/material.dart';
-class Home extends StatelessWidget{
+
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-        child:Container(
-      alignment: Alignment.bottomRight,
-      color: Colors.deepPurple,
-     // width: 421.5,
-      //height: 321.5,
-      margin: EdgeInsets.only(right:15.0 , top: 20.5),
-      child: Text("Abracadabra",
-         textDirection: TextDirection.ltr,
-      ),
-    )
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            // width: 421.5,
+            //height: 321.5,
+            margin: EdgeInsets.all(15.0),
+            child: Column(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Kenya Airways",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 30.0,
+                        fontFamily: 'Ubuntu',
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white),
+                  )),
+                  Expanded(
+                    child: Text("From London to Nairobi",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'Ubuntu',
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white)),
+                  ),
+                ]
+                ),
+                Row(children: <Widget>[
+                  Expanded(
+                      child: Text(
+                        "Fly 540",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 30.0,
+                            fontFamily: 'Ubuntu',
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white),
+                      )),
+                  Expanded(
+                    child: Text("From New Delhi to Eldoret",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'Ubuntu',
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white)),
+                  ),
+                ]
+                ),
+              FlightImageAsset()
 
+              ],
+            )));
+  }
+}
 
-        );
+class FlightImageAsset extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage =AssetImage('img/gate.jpg');
+    Image image = Image(image: assetImage);
 
-
-
-
+    return Container(child: image,);
   }
 
 }
