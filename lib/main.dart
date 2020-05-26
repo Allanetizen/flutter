0c5e7d651640d6dbb24ea9ac03f4ea094723e95e
Allanetizen <allankosgei747@gmail.com>
2020-05-27 01:33:14 +0300

    Colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,28 @@ void main() => runApp(MaterialApp(
     title: "Cool App",
     home: Scaffold( //wrapper to the widgets /layouts
       appBar: AppBar(
-        title: Text('Las Veges'),
+        title: Text(
+            'Las Veges',
+          style: TextStyle(
+            fontSize: 20.0 ,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.black,
+            fontFamily: 'Ubuntu',
+
+
+          ) ,
+        ),
         centerTitle: true, //centers the above text
+        backgroundColor: Colors.green,
       ),
       body: Center(
         child: Text('Hello Veges'),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: (){},
         child: Text('Click!'),
+        backgroundColor: Colors.green,
       ),
     ),
   )
