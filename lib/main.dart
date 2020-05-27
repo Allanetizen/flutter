f027c2ada7932c55ec9f00c52359eed2803d5f7e
Allanetizen <allankosgei747@gmail.com>
2020-05-27 04:05:45 +0300

    stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    title: "Cool App",
-    home: Scaffold( //wrapper to the widgets /layouts
-      appBar: AppBar(
-        title: Text(
-            'Las Veges',
-          style: TextStyle(
-            fontSize: 20.0 ,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.black,
-            fontFamily: 'Ubuntu',
+void main() =>
+    runApp(MaterialApp(
+        title: "Cool App",
+        home: Home()
+    )
+    );
+
+class Home extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+return Scaffold( //wrapper to the widgets /layouts
+appBar: AppBar(
+title: Text(
+'Las Veges',
+style: TextStyle(
+fontSize: 20.0 ,
+fontWeight: FontWeight.bold,
+letterSpacing: 2,
+color: Colors.black,
+fontFamily: 'Ubuntu',
 
 
-          ) ,
-        ),
-        centerTitle: true, //centers the above text
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Text(
-            'Hello Veges',
-        style: TextStyle(
-          fontFamily: 'Ubuntu'
-        ),),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Text('Click!',
-        style: TextStyle(
-          fontFamily: 'Ubuntu'
-        ),),
-        backgroundColor: Colors.green,
-      ),
-    ),
-  )
+) ,
+),
+centerTitle: true, //centers the above text
+backgroundColor: Colors.green,
+),
+body: Center(
+child: Text(
+'Hello Veges',
+style: TextStyle(
+fontFamily: 'Ubuntu'
+),),
+),
+floatingActionButton: FloatingActionButton(
+onPressed: (){},
+child: Text('Click!',
+style: TextStyle(
+fontFamily: 'Ubuntu'
+),),
+backgroundColor: Colors.green,
+),
 );
+}
+}
+
