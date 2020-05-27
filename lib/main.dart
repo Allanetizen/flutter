809dd84805c055651465b85694c322810a44d28f
Allanetizen <allankosgei747@gmail.com>
2020-05-27 20:52:17 +0300

    flat and raised buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,47 @@
-import 'package:flutter/material.dart';
+                import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MaterialApp(
-        title: "Cool App",
-        home: Home()
-    )
-    );
+                void main() => runApp(MaterialApp(title: "Cool App", home: Home()));
 
-class Home extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return Scaffold( //wrapper to the widgets /layouts
-appBar: AppBar(
-title: Text(
-'Healthy Treats',
-style: TextStyle(
-fontSize: 20.0 ,
-fontWeight: FontWeight.bold,
-letterSpacing: 2,
-color: Colors.black,
-fontFamily: 'Ubuntu',
+                class Home extends StatelessWidget {
+                  @override
+                  Widget build(BuildContext context) {
+                    return Scaffold(
+                      //wrapper to the widgets /layouts
+                      appBar: AppBar(
+                        title: Text(
+                          'Healthy Treats',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2,
+                            color: Colors.black,
+                            fontFamily: 'Ubuntu',
+                          ),
+                        ),
+                        centerTitle: true, //centers the above text
+                        backgroundColor: Colors.green,
+                      ),
+                      body: Center(
+                        child: RaisedButton.icon(
+                          //raised button has a shadow unlike FlatButton
+                          onPressed: () {
+                            },
+                          icon: Icon(
+                              Icons.add ,
+                          ),
+                          label: Text('Add a category'),
+                          color: Colors.green,
+                        ),
+                      ),
 
-
-) ,
-),
-centerTitle: true, //centers the above text
-backgroundColor: Colors.green,
-),
-body: Center(
-    child: Icon(
-        Icons.local_hospital,
-         color: Colors.red,
-        size:40.0,
-    ),
-
-),
-
-floatingActionButton: FloatingActionButton(
-onPressed: (){},
-child: Text('Click!',
-style: TextStyle(
-fontFamily: 'Ubuntu'
-),),
-backgroundColor: Colors.green,
-),
-);
-}
-}
-
+                      floatingActionButton: FloatingActionButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Click!',
+                          style: TextStyle(fontFamily: 'Ubuntu'),
+                        ),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+                  }
+                }
