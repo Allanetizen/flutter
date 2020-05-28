0f68547c765cf9da07bbe87085ba8da57fd65505
Allanetizen <allankosgei747@gmail.com>
2020-05-28 10:00:33 +0300

    margin and padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-                import 'package:flutter/material.dart';
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
                 void main() => runApp(MaterialApp(title: "Cool App", home: Home()));
 
@@ -7,6 +10,7 @@
                   Widget build(BuildContext context) {
                     return Scaffold(
                       //wrapper to the widgets /layouts
+
                       appBar: AppBar(
                         title: Text(
                           'Healthy Treats',
@@ -21,24 +25,19 @@
                         centerTitle: true, //centers the above text
                         backgroundColor: Colors.green,
                       ),
-                      body: Center(
-                        child: IconButton(
-                          onPressed: (){
-                            print('Clicked dyd');
-                          },
-                          icon:Icon(Icons.add_a_photo) ,
-                          color: Colors.red
-                        ),
-                        ),
-                      
+                      body: Container (
+                        padding: EdgeInsets.fromLTRB(10.0, 15.0, 20.0, 12.0),
+                       margin: EdgeInsets.all(30.0), //padding widget:can be used to enclose the text/no color
+                        color: Colors.greenAccent,
+                        child: Text('Hello, eat healthy'),
 
+
+                      ),
                       floatingActionButton: FloatingActionButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Click!',
-                          style: TextStyle(fontFamily: 'Ubuntu'),
-                        ),
+                        onPressed: (){},
+                        child: Text('Click!'),
                         backgroundColor: Colors.green,
+
                       ),
                     );
                   }
